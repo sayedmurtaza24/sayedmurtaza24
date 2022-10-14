58ae76e5174a71e7fd81072c574a2489f368862a
Sayed Murtaza <sayedmurtazamuttahary@gmail.com>
2022-10-14 12:12:54 +0200

    contact form done using emailjs

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import emailjs from '@emailjs/browser'
 import './Contact.css'
+import { useState } from 'react';
 
 function Contact() {
+    const nameInput = useRef(null);
+    const emailInput = useRef(null);
+    const messageInput = useRef(null);
+
+    const [sent, setSent] = useState(false)
+
+    const initializeEmailJs = async () => {
+        emailjs.init("JMo4-OegvzXJb8jek")
+    }
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+        if (sent) return;
+        emailjs
+            .send("default_service", "template_ujrvie6", {
+                message: messageInput.current.value,
+                from_name: nameInput.current.value,
+                from_email: emailInput.current.value,
+            })
+            .then(() => setSent(true));
+    }
+
+    useEffect(() => {
+        initializeEmailJs();
+    }, []);
+
     return (
         <section className="contact-section">
             <div className="contact-section__text">
@@ -12,17 +40,35 @@ function Contact() {
                     <h2 className="form-header">Contact Me</h2>
                     <div className="form-part">
                         <label className="form-label" htmlFor="name">Your Name:</label>
-                        <input placeholder='Name' className="form-input" type="text" name="name" id="name" />
+                        <input ref={nameInput}
+                            placeholder='Name'
+                            className="form-input"
+                            type="text"
+                            name="name"
+                            id="name" />
                     </div>
                     <div className="form-part">
                         <label className="form-label" htmlFor="name">Your Email:</label>
-                        <input placeholder='Email' className="form-input" type="text" name="email" id="email" />
+                        <input ref={emailInput}
+                            placeholder='Email'
+                            className="form-input"
+                            type="text"
+                            name="email"
+                            id="email" />
                     </div>
                     <div className="form-part">
                         <label className="form-label" htmlFor="message">Message:</label>
-                        <textarea placeholder='Message...' className="form-input" id="message" name="message" rows="4" cols="50"></textarea>
+                        <textarea ref={messageInput}
+                            placeholder='Message...'
+                            className="form-input"
+                            id="message"
+                            name="message"
+                            rows="4" cols="50" />
                     </div>
-                    <button className="form-button">Send</button>
+                    <div className="form-control">
+                        <button onClick={sendEmail} className="form-button">Send</button>
+                        {sent && <p className="sent-success">Sent 🤞</p>}
+                    </div>
                 </form>
             </div>
         </section>
